97fe63a13936d0c1167c32343a8ce3df4887e352
Ganesh-1907 <ganesh.bora@artofliving.org>
2026-02-24 22:47:32 +0530

    products add ml and gms and also dropdown options moistrizer and night cream

--- src/models/Product.js
+++ src/models/Product.js
@@ -25,7 +25,7 @@ const productSchema = new mongoose.Schema(
 
     category: {
       type: String,
-      enum: ["facewash", "serums", "creams"],
+      enum: ["facewash", "serums", "creams", "moisturizer", "night-cream"],
       required: true,
     },
 
